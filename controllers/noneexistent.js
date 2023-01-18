fb4c2e813739241ad1b29f63291cb75751b10cb1
Totter <tottercotter@gmail.com>
2023-01-19 01:42:49 +0600

    fc

--- controllers/noneexistent.js
+++ controllers/noneexistent.js
@@ -2,7 +2,7 @@ const NotFoundError = (req, res) => {
   const err = new Error('NotFoundError');
   err.name = 'NotFoundError';
   err.message = 'NotFoundError';
-  res.status(404).send(err.message);
+  res.status(404).send(err);
 };
 
 module.exports = { NotFoundError };
